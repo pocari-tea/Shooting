1778f97323a086df31666db744276a792305210e
정훈 <41151326+pocari-tea@users.noreply.github.com>
2020-09-01 12:26:54 +0900

    beta
    shooting

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector3.down;
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
